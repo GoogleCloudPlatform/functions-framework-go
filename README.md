ae063640870567f17a7b72dcb4d5707ce58add6e
Julie Hockett <11913400+juliehockett@users.noreply.github.com>
2019-10-16 14:10:30 -0700

    Framework and tests (#1)
    Initial library implementation.

--- README.md
+++ README.md
@@ -1,1 +1,189 @@
+# Functions Framework for Go
 
+An open source FaaS (Function as a Service) framework for writing portable
+Go functions, brought to you by the Google Cloud Functions team.
+
+The Functions Framework lets you write lightweight functions that run in many
+different environments, including:
+
+*   [Google Cloud Functions](https://cloud.google.com/functions/)
+*   Your local development machine
+*   [Knative](https://github.com/knative/)-based environments
+*   [Google App Engine](https://cloud.google.com/appengine/docs/go/)
+*   [Google Cloud Run](https://cloud.google.com/run/docs/quickstarts/build-and-deploy)
+
+The framework allows you to go from:
+
+```golang
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello, World!")
+}
+```
+
+To:
+
+```sh
+curl http://my-url
+# Output: Hello, World!
+```
+
+All without needing to worry about writing an HTTP server or request
+handling logic.
+
+# Features
+
+*   Spin up a local development server for quick testing with little extra code
+*   Invoke a function in response to a request
+*   Automatically unmarshal events conforming to the
+    [CloudEvents](https://cloudevents.io/) spec
+*   Portable between serverless platforms
+
+# Quickstart: Hello, World on your local machine
+
+1. Create the necessary directories.
+	```sh
+	mkdir -p hello/cmd
+	cd hello
+	```
+
+1. Create a Go module:
+	```sh
+	go mod init example.com/hello
+	```
+
+	> Note: You can use a different module name rather than `example.com/hello`.
+
+1. Create a `function.go` file with the following contents:
+	```golang
+	package hello
+
+	import (
+		"net/http"
+		"fmt"
+	)
+
+	// HelloWorld writes "Hello, World!" to the HTTP response.
+	func HelloWorld(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!")
+	}
+	```
+
+	> Note that you can use any file name or package name (convention is to make
+	package name same as directory name).
+
+1. Now go to the `cmd` subdirectory.
+	```sh
+	cd cmd
+	```
+
+1. Create a `main.go` file with the following contents:
+	```golang
+	package main
+
+	import (
+		"fmt"
+		"log"
+		"net/http"
+		"os"
+
+		"cloud.google.com/go/functions/framework"
+		"example.com/hello"
+	)
+
+	func main() {
+		if err := framework.RegisterHTTPFunction("/", hello.HelloWorld); err != nil {
+			log.Fatalf("framework.RegisterHTTPFunction: %v\n", err)
+		}
+		// Use PORT environment variable, or default to 8080.
+        port := "8080"
+        if envPort := os.Getenv("PORT"); envPort != "" {
+                port = envPort
+        }
+
+		if err := framework.Start(port); err != nil {
+			log.Fatalf("framework.Start: %v\n", err)
+		}
+	}
+	```
+
+1. Start the local development server:
+	```sh
+	go build
+	./cmd
+	Function serving...
+	URL: http://localhost:8080/
+	```
+
+2. Send requests to this function using `curl` from another terminal window:
+	```sh
+	curl localhost:8080
+	# Output: Hello, World!
+	```
+
+# Run your function on serverless platforms
+
+## Google Cloud Functions
+
+You cannot deploy main packages to Google Cloud Functions. You need to go back to the parent directory
+in which your function code is.
+
+```sh
+cd ..
+```
+
+and you can deploy it from your local machine using the `gcloud` command-line tool.
+[Check out the Cloud Functions quickstart](https://cloud.google.com/functions/docs/quickstart).
+
+## Container environments based on Knative
+
+The Functions Framework is designed to be compatible with Knative environments.
+Just build and deploy your container to a Knative environment. Note that your app needs to listen
+`PORT` environment variable per [Knative runtime contract](https://github.com/knative/serving/blob/master/docs/runtime-contract.md#inbound-network-connectivity).
+
+# Configure the Functions Framework
+
+If you're deploying to Google Cloud Functions, you don't need to worry about writing a
+`package main`. But if you want to run your function locally (e.g., for local development),
+you may want to configure the port, the function to be executed, and the function signature type
+(which specifies event unmarshalling logic). You can do this by modifying the `main.go`
+file described above:
+
+To select a port, set the `$PORT` environment variable when running.
+
+```sh
+PORT=8000 ./cmd
+```
+
+To select a function, pass your function to `framework.RegisterHTTPFunction` in the second variable.
+
+```golang
+framework.RegisterHTTPFunction("/", myFunction);
+```
+
+If your function handles events, use `framework.RegisterEventFunction` instead of `framework.RegisterHTTPFunction`.
+
+```golang
+framework.RegisterEventFunction("/", eventFunction);
+
+func eventFunction(ctx context.Context, e myEventType){
+	// function logic
+}
+```
+
+> Note that the first parameter to a function that handles events has to be `context.Context`
+and the type of second parameter needs to be a type of an unmarshallable event.
+
+# Enable Cloud Events
+
+The Functions Framework can unmarshal to custom structs, and provides support for 
+unmarshalling an incoming [CloudEvents](http://cloudevents.io) payload to a
+`cloudevents.Event` object. These will be passed as arguments to your function when it receives a request.
+Note that your function must use the event-style function signature.
+
+```golang
+func CloudEventsFunction(ctx context.Context, e cloudevents.Event) {
+    // Do something with event.Context and event.Data (via event.DataAs(foo)).
+}
+```
+
+To learn more about CloudEvents, see the [Go SDK for CloudEvents](https://github.com/cloudevents/sdk-go).
---
+++ framework/framework.go
@@ -0,0 +1,202 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package framework is a Functions Framework implementation for Go. It allows you to register
+// HTTP and event functions, then start an HTTP server serving those functions.
+package framework
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"reflect"
+	"runtime/debug"
+	"strings"
+)
+
+const (
+	functionStatusHeader = "X-Google-Status"
+	crashStatus          = "crash"
+	errorStatus          = "error"
+)
+
+// RegisterHTTPFunction registers fn as an HTTP function.
+func RegisterHTTPFunction(path string, fn func(http.ResponseWriter, *http.Request)) {
+	registerHTTPFunction(path, fn, http.DefaultServeMux)
+}
+
+// RegisterEventFunction registers fn as an event function. The function must have two arguments, a
+// context.Context and a struct type depending on the event, and return an error. If fn has the
+// wrong signature, RegisterEventFunction panics.
+func RegisterEventFunction(path string, fn interface{}) {
+	registerEventFunction(path, fn, http.DefaultServeMux)
+}
+
+// Start serves an HTTP server with registered function(s).
+func Start(port string) error {
+	// Check if we have a function resource set, and if so, log progress.
+	if os.Getenv("K_SERVICE") == "" {
+		fmt.Println("Serving function...")
+	}
+	return http.ListenAndServe(":"+port, nil)
+}
+
+func registerHTTPFunction(path string, fn func(http.ResponseWriter, *http.Request), h *http.ServeMux) {
+	h.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		// TODO(b/111823046): Remove following once Cloud Functions does not need flushing the logs anymore.
+		// Force flush of logs after every function trigger.
+		defer fmt.Println()
+		defer fmt.Fprintln(os.Stderr)
+		defer func() {
+			if r := recover(); r != nil {
+				writeHTTPErrorResponse(w, http.StatusInternalServerError, crashStatus, fmt.Sprintf("Function panic: %v\n\n%s", r, debug.Stack()))
+			}
+		}()
+		fn(w, r)
+	})
+}
+
+func registerEventFunction(path string, fn interface{}, h *http.ServeMux) {
+	validateEventFunction(fn)
+	h.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		// TODO(b/111823046): Remove following once Cloud Functions does not need flushing the logs anymore.
+		// Force flush of logs after every function trigger.
+		defer fmt.Println()
+		defer fmt.Fprintln(os.Stderr)
+		defer func() {
+			if r := recover(); r != nil {
+				writeHTTPErrorResponse(w, http.StatusInternalServerError, crashStatus, fmt.Sprintf("Function panic: %v\n\n%s", r, debug.Stack()))
+			}
+		}()
+		handleEventFunction(w, r, fn)
+	})
+}
+
+func validateEventFunction(fn interface{}) {
+	ft := reflect.TypeOf(fn)
+	if ft.NumIn() != 2 {
+		panic(fmt.Sprintf("expected function to have two parameters, found %d", ft.NumIn()))
+	}
+	var err error
+	errorType := reflect.TypeOf(&err).Elem()
+	if ft.NumOut() != 1 || !ft.Out(0).AssignableTo(errorType) {
+		panic("expected function to return only an error")
+	}
+	var ctx context.Context
+	ctxType := reflect.TypeOf(&ctx).Elem()
+	if !ctxType.AssignableTo(ft.In(0)) {
+		panic("expected first parameter to be context.Context")
+	}
+}
+
+func isBinaryCloudEvent(r *http.Request) bool {
+	ceReqHeaders := []string{"Ce-Type", "Ce-Specversion", "Ce-Source", "Ce-Id"}
+	for _, h := range ceReqHeaders {
+		if _, ok := r.Header[http.CanonicalHeaderKey(h)]; ok {
+			return false
+		}
+	}
+	return true
+}
+
+func handleEventFunction(w http.ResponseWriter, r *http.Request, fn interface{}) {
+	body := readHTTPRequestBody(w, r)
+	if body == nil {
+		// No body, error has already been written.
+		return
+	}
+
+	if isBinaryCloudEvent(r) {
+		runUserFunction(w, r, body, fn)
+		return
+	}
+
+	// Otherwise, we have to parse the request to extract the context and the body for the data.
+	event := make(map[string]interface{})
+	event["data"] = string(body)
+	for k, v := range r.Header {
+		k = strings.ToLower(k)
+		if !strings.HasPrefix(k, "ce-") {
+			continue
+		}
+		k = strings.TrimPrefix(k, "ce-")
+		if len(v) != 1 {
+			writeHTTPErrorResponse(w, http.StatusBadRequest, crashStatus, fmt.Sprintf("Too many header values: %s", k))
+			return
+		}
+		var mapVal map[string]interface{}
+		if err := json.Unmarshal([]byte(v[0]), &mapVal); err != nil {
+			// If there's an error, represent the field as the string from the header. Errors will be caught by the event constructor if present.
+			event[k] = v[0]
+		} else {
+			// Otherwise, represent the unmarshalled map value.
+			event[k] = mapVal
+		}
+	}
+
+	// We don't want any escaping to happen here.
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	enc.SetEscapeHTML(false)
+	err := enc.Encode(event)
+	if err != nil {
+		writeHTTPErrorResponse(w, http.StatusBadRequest, crashStatus, fmt.Sprintf("Unable to construct event: %s", err.Error()))
+		return
+	}
+
+	runUserFunction(w, r, buf.Bytes(), fn)
+}
+
+func readHTTPRequestBody(w http.ResponseWriter, r *http.Request) []byte {
+	if r.Body == nil {
+		writeHTTPErrorResponse(w, http.StatusBadRequest, crashStatus, "Request body not found")
+		return nil
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		writeHTTPErrorResponse(w, http.StatusUnsupportedMediaType, crashStatus, fmt.Sprintf("Could not read request body: %s", err.Error()))
+		return nil
+	}
+
+	return body
+}
+
+func runUserFunction(w http.ResponseWriter, r *http.Request, data []byte, fn interface{}) {
+	argVal := reflect.New(reflect.TypeOf(fn).In(1))
+	if err := json.Unmarshal(data, argVal.Interface()); err != nil {
+		writeHTTPErrorResponse(w, http.StatusUnsupportedMediaType, crashStatus, fmt.Sprintf("Error while converting event data: %s", err.Error()))
+		return
+	}
+
+	userFunErr := reflect.ValueOf(fn).Call([]reflect.Value{
+		reflect.ValueOf(r.Context()),
+		argVal.Elem(),
+	})
+	if userFunErr[0].Interface() != nil {
+		writeHTTPErrorResponse(w, http.StatusInternalServerError, errorStatus, fmt.Sprintf("Function error: %v", userFunErr[0]))
+		return
+	}
+}
+
+func writeHTTPErrorResponse(w http.ResponseWriter, statusCode int, status, msg string) {
+	w.Header().Set(functionStatusHeader, status)
+	w.WriteHeader(statusCode)
+	fmt.Fprintf(os.Stderr, msg)
+	fmt.Fprintf(w, msg)
+}
