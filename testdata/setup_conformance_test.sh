d2f3622a272c8817e54513faf310414f88f7b324
Julie Hockett <juliehockett@google.com>
2020-08-14 10:36:53 -0700

    vendor if sha isn't available

---
+++ testdata/setup_conformance_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+TESTDATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VENDOR_DIR="${TESTDATA_DIR?}/vendor/github.com/GoogleCloudPlatform/functions-framework-go"
+FF_DIR="$(dirname "${TESTDATA_DIR}")"
+
+function goget {
+  pushd ${TESTDATA_DIR?}
+  go get github.com/GoogleCloudPlatform/functions-framework-go@${GITHUB_SHA?}
+  [ $? -eq 0 ] && popd || popd && return 1
+}
+
+function vendor {
+  pushd ${TESTDATA_DIR?}
+  go mod vendor
+  rm -rf ${VENDOR_DIR?}
+  mkdir -p ${VENDOR_DIR?}
+  cp "${FF_DIR?}/go.mod" ${VENDOR_DIR?}
+  cp -r "${FF_DIR?}/funcframework" ${VENDOR_DIR?}
+  popd
+}
+
+goget || vendor
